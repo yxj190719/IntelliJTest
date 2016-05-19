d5ac5f7bfff4e86d5a4c41712bb39320903c74ec
xinjianyao <xinjianyao@carfax.com>
2016-05-19 10:55:19 -0500

    test1

---
+++ primary/src/AnotherOne.java
@@ -0,0 +1,8 @@
+/**
+ * Created by xinjianyao on 5/19/2016.
+ */
+class AnotherOne {
+    int a=hashCode();
+
+
+}
---
+++ primary/src/Test.java
@@ -0,0 +1,5 @@
+/**
+ * Created by xinjianyao on 5/19/2016.
+ */
+public class Test {
+}
